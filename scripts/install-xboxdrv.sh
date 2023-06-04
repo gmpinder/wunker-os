120b881caddae2cb5b3ec02cbe54e44d46f82836
gmpinder <gmpinder@users.noreply.github.com>
2023-06-04 01:10:17 -0400

    Fix typo in package

--- scripts/install-xboxdrv.sh
+++ scripts/install-xboxdrv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-dnf install git gcc-c++ pkgconfig libusb1-devel boost-devel systemd-devel dbus-devel python3-scon
+dnf install git gcc-c++ pkgconfig libusb1-devel boost-devel systemd-devel dbus-devel python3-scons
 cd ~
 mkdir src
 cd src
